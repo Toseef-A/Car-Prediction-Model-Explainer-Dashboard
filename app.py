fba822428c049eea51fe60c0359de20ce0a02076
Toseef Ashraf Parveen <147708587+Toseef-A@users.noreply.github.com>
2025-07-22 00:38:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import joblib
 from explainerdashboard import RegressionExplainer, ExplainerDashboard
+import os
 
 # Load model and data
 model = joblib.load("best_xgb_model.pkl")
 X_test_scaled = joblib.load("X_test_scaled.pkl")
 y_test = joblib.load("y_test.pkl")
 
-# Create the explainer using the scaled test data
+# Create the explainer
 explainer = RegressionExplainer(model, X_test_scaled, y_test)
 
-# Launch the dashboard
+# Run the dashboard on the correct port for Render
 if __name__ == "__main__":
-    ExplainerDashboard(explainer).run()
+    port = int(os.environ.get("PORT", 8050))
+    ExplainerDashboard(explainer).run(port=port)
