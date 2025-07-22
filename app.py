b7ac386f0c8555d0feecdd629496b4c9d81614c9
Toseef Ashraf Parveen <147708587+Toseef-A@users.noreply.github.com>
2025-07-22 13:40:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import joblib
-from explainerdashboard import RegressionExplainer, ExplainerDashboard
 import os
+from explainerdashboard import RegressionExplainer, ExplainerDashboard
 
 # Load model and data
 model = joblib.load("best_xgb_model.pkl")
 X_test_scaled = joblib.load("X_test_scaled.pkl")
 y_test = joblib.load("y_test.pkl")
 
-# Create explainer
-explainer = RegressionExplainer(model, X_test_scaled, y_test)
+# Create explainer (limit to 1000 rows, disable SHAP interactions to speed up)
+explainer = RegressionExplainer(
+    model,
+    X_test_scaled[:1000],
+    y_test[:1000],
+    shap_interaction=False
+)
 
-# Bind to dynamic port + 0.0.0.0 for Render to detect
+# Bind to dynamic port and 0.0.0.0 for Render
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
+    port = int(os.environ.get("PORT", 8100))
+    print(f"Starting dashboard on port {port}...")
     ExplainerDashboard(explainer).run(port=port, host="0.0.0.0")
