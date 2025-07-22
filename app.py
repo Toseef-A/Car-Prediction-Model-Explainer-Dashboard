2ba50ee367172821bae4c1ca1364fa4073c1bfd4
Toseef Ashraf Parveen <147708587+Toseef-A@users.noreply.github.com>
2025-07-22 15:06:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from dash import Dash, html
+import pandas as pd
+import joblib
 import os
+from explainerdashboard import RegressionExplainer, ExplainerDashboard
 
-app = Dash(__name__)
-app.layout = html.Div("Test App")
+# Load model and data (limit to 1000 rows for faster load)
+model = joblib.load("best_xgb_model.pkl")
+X_test_scaled = joblib.load("X_test_scaled.pkl")
+y_test = joblib.load("y_test.pkl")
+
+explainer = RegressionExplainer(
+    model,
+    X_test_scaled[:100],
+    y_test[:100]
+)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    print(f"Running test app on port {port}")
-    app.run_server(host="0.0.0.0", port=port)
+    print(f"Starting ExplainerDashboard on port {port}")
+    ExplainerDashboard(
+        explainer,
+        shap_interaction=False,  # disables SHAP interaction values
+        no_permutations=True,    # skips permutation importance
+        simple=True              # loads only basic tabs
+    ).run(port=port, host="0.0.0.0")
